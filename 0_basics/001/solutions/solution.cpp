3e3f011a359b8914a9ae6a31281c4f0d7fbd3470
Mihail Croitor <mcroitor@gmail.com>
2023-06-01 15:50:33 +0300

    some fixes

---
+++ 0_basics/001/solutions/solution.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file solution.cpp
+ * @author Mihail Croitor
+ * @brief `proxy` task solution
+ * @version 0.1
+ * @date 2023-06-01
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+int main() {
+    int a;
+    std::cin >> a;
+    std::cout << a;
+    return 0;
+}
