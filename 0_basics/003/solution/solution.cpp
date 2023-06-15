7c2c2fd82692d60be4bfc8669939303a8f51f3bc
Alexandr Voronin <alexandrvoronin62@gmail.com>
2023-06-15 11:32:38 +0300

    cpp solution for 003,004,005,006

---
+++ 0_basics/003/solution/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file solution.cpp
+ * @author Alexandr Voronin
+ * @brief `proxy 2` task solution
+ * @version 0.1
+ * @date 2023-06-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a + b << "" << endl;
+    return 0;
+}
