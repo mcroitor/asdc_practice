3b911e25ff276c50e533cf6162dedc00bc57ef5c
piotrGrigoret <123254470+piotrGrigoret@users.noreply.github.com>
2023-09-22 21:42:38 +0300

    Add files via upload

---
+++ 0_basics/015/solution.php
@@ -0,0 +1,27 @@
+
+<?php
+
+
+// 15
+
+/*
+ * @file solution.php
+ * @author Grigoret Piotr
+ * @brief `proxy` task solution
+ * @version 0.1
+ * @date 2023-09-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+$x = $argv[1];
+
+function isNumberPowerOfTwo($x){
+
+    return $x % 2 == 0; 
+}
+
+isNumberPowerOfTwo($x);
+echo $result;
+?>    
