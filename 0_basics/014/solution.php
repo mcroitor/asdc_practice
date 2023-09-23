c88985dc9ffa29390496f2e720fac0456fa908f9
piotrGrigoret <123254470+piotrGrigoret@users.noreply.github.com>
2023-09-23 18:01:01 +0300

    Create solution.php

---
+++ 0_basics/014/solution.php
@@ -0,0 +1,31 @@
+<?php
+
+
+
+/*
+ * @file solution.php
+ * @author Grigoret Piotr
+ * @brief `proxy` task solution
+ * @version 0.1
+ * @date 2023-09-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+echo "Enter data: ";
+
+$c = readline();
+
+function isNumber($c){
+
+    return is_numeric($c); 
+}
+
+$result = isNumber($c);
+echo $result;
+
+
+
+
+?>    
