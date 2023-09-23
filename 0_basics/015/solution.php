c046c27015970ded3805ee2a3a591a92eba9686f
piotrGrigoret <123254470+piotrGrigoret@users.noreply.github.com>
2023-09-23 18:01:59 +0300

    Create solution.php

---
+++ 0_basics/015/solution.php
@@ -0,0 +1,31 @@
+
+<?php
+
+
+
+
+/*
+ * @file solution.php
+ * @author Grigoret Piotr
+ * @brief `proxy` task solution
+ * @version 0.1
+ * @date 2023-09-22
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+echo "Enter data: ";
+
+
+$x = readline();
+
+function isNumberPowerOfTwo($x){
+
+    return $x % 2 == 0; 
+}
+
+$result = isNumberPowerOfTwo($x);
+echo $result;
+
+?>    
