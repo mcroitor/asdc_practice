5024b65b368365e17dae29e7cfc5ac1c4e0de809
Mihail Croitor <mihail.croitor@usm.md>
2025-12-19 13:10:12 +0200

    added stack and deque problems

--- 1_structures/001/solutions/solution.cpp
+++ 1_structures/001/solutions/solution.cpp
@@ -8,12 +8,22 @@ class Student
     std::string _group;
 
 public:
-    Student(std::string _first_name, std::string _last_name, std::string _group);
-    Student operator = (const Student &student);
+    Student(std::string _first_name, std::string _last_name, std::string _group) 
+        : _first_name(std::move(_first_name)), 
+          _last_name(std::move(_last_name)), 
+          _group(std::move(_group)) 
+    {};
+    Student& operator = (const Student &student) = default;
 
-    std::string firstName() const;
-    std::string lastName() const;
-    std::string group() const;
+    std::string firstName() const {
+        return _first_name;
+    }
+    std::string lastName() const {
+        return _last_name;
+    }
+    std::string group() const {
+        return _group;
+    }
 };
 
 std::ostream & operator << (std::ostream &os, const Student &student)
---
+++ 1_structures/003/solutions/solution.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+int main() {
+    std::string s;
+    std::cin >> s;
+    std::stack<char> stk;
+
+    for (char ch : s) {
+        if (ch == '(' || ch == '{' || ch == '[') {
+            stk.push(ch);
+        } else {
+            if (stk.empty()) {
+                std::cout << "NO\n";
+                return 0;
+            }
+            char top = stk.top();
+            stk.pop();
+            if ((ch == ')' && top != '(') ||
+                (ch == '}' && top != '{') ||
+                (ch == ']' && top != '[')) {
+                std::cout << "NO\n";
+                return 0;
+            }
+        }
+    }
+
+    if (stk.empty()) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+    return 0;
+}
